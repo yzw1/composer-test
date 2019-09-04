e4665b38f26d54c9df1971d47316568f475cadd6
sinker.yin <sinker.yin@etocrm.net>
2019-09-04 11:33:58 +0800

    ''

---
+++ src/composer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sinker.yin
+ * Date: 2019/9/4
+ * Time: 10:39
+ */
+namespace Composer;
+
+class Test
+{
+    public function save()
+    {
+        echo 'composer-test';
+    }
+}
